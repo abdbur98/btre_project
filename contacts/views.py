e37a0281d4d0b9890ffe0447a1559ac693755dbd
abdallaburghli <abdallabur@gmail.com>
2020-10-09 10:28:14 +0300

    second commit

---
+++ contacts/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render , redirect
+from .models import Contact
+from django.contrib import messages
+from django.core.mail import send_mail
+
+# Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+       listing_id = request.POST['listing_id']
+       listing = request.POST['listing']
+       name = request.POST['name']
+       email = request.POST['email']
+       phone = request.POST['phone']
+       message = request.POST['message']
+       user_id = request.POST['user_id']
+       realtor_email = request.POST['realtor_email']
+
+       # check if user has already made an inquery
+       if request.user.is_authenticated:
+           user_id = request.user.id
+           has_contacted = Contact.objects.all().filter( listing_id =listing_id,user_id=user_id)
+           if has_contacted:
+                messages.error(request,'you already made this inquery')
+                return redirect('/listings/'+listing_id)
+
+       contact = Contact(listing=listing,listing_id=listing_id , name=name , email = email , phone = phone,
+            message=message , user_id = user_id)
+
+       contact.save()
+
+       send_mail(
+           'Property Listing Inquiry',
+           'There has been an inquery for '+listing+'. Sign into the admin panel for more info',
+           'emial@gmail.com',
+           [realtor_email],
+           fail_silently=False
+       )
+
+       return redirect('/listings/'+listing_id)
+
+
+
